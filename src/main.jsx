9674b836fc4398d98f0cca6b61ea1830231083d1
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-01-30 15:19:28 +0700

    add boostrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./redux/store";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
