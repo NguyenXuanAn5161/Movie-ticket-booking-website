663e0f7f7ff86570334d60c1abb123e980332e81
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-03 01:13:59 +0700

    fix: add breadcrumb

---
+++ src/components/Breadcrumb/CustomBreadcrumb.jsx
@@ -0,0 +1,111 @@
+import { Breadcrumb } from "antd";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function CustomBreadcrumb(props) {
+  const location = useLocation();
+
+  const items = [
+    {
+      path: "/admin",
+      title: "",
+      children: [
+        {
+          path: "/dashboard",
+          title: "Tổng quan",
+        },
+        {
+          path: "/order",
+          title: "Hóa đơn",
+        },
+        {
+          path: "/user",
+          title: "Người dùng",
+        },
+        {
+          path: "/movie",
+          title: "Phim",
+        },
+        {
+          path: "/cinema",
+          title: "Rạp chiếu",
+          children: [
+            {
+              path: "/room",
+              title: "Phòng chiếu",
+              children: [
+                {
+                  path: "/seat",
+                  title: "Ghế",
+                },
+                {
+                  path: "/seatType",
+                  title: "Loại ghế",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          path: "/food",
+          title: "Đồ ăn",
+        },
+        {
+          path: "/foodCategories",
+          title: "Loại đồ ăn",
+        },
+        {
+          path: "/promotion",
+          title: "Khuyến mãi",
+        },
+      ],
+    },
+  ];
+
+  const getItemByPath = (path, itemList) => {
+    for (let i = 0; i < itemList.length; i++) {
+      const item = itemList[i];
+      if (item.path === path) {
+        return item;
+      }
+      if (item.children) {
+        const foundItem = getItemByPath(path, item.children);
+        if (foundItem) {
+          return foundItem;
+        }
+      }
+    }
+    return null;
+  };
+
+  const currentPath = location.pathname;
+
+  const itemRender = (route, params, routes, paths) => {
+    const last = routes.indexOf(route) === routes.length - 1;
+    const item = getItemByPath(route.path, items);
+    console.log("item: ", item);
+
+    // Không render title cho mục đầu tiên
+    if (route.path === "/admin") {
+      return null;
+    }
+
+    return last ? (
+      <span>{item ? item.title : null}</span>
+    ) : (
+      <Link to={paths.join("/")}>{item ? item.title : null}</Link>
+    );
+  };
+
+  return (
+    <Breadcrumb
+      itemRender={itemRender}
+      items={currentPath.split("/").map((path) => ({
+        path: `/${path}`,
+        title: console.log("return: ", getItemByPath(`/${path}`, items)),
+      }))}
+    />
+  );
+}
+
+export default CustomBreadcrumb;
