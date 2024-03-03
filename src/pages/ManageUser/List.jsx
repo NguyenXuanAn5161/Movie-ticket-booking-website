c19f166885d0d529c0ff435b4dfea3083e557f5a
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-03 16:07:35 +0700

    fix: fix name function

---
+++ src/pages/ManageUser/List.jsx
@@ -0,0 +1,313 @@
+import {
+  Button,
+  Col,
+  Popconfirm,
+  Row,
+  Table,
+  message,
+  notification,
+} from "antd";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import {
+  AiOutlineDelete,
+  AiOutlineExport,
+  AiOutlineImport,
+  AiOutlinePlus,
+  AiOutlineReload,
+} from "react-icons/ai";
+import { BsEye } from "react-icons/bs";
+import { CiEdit } from "react-icons/ci";
+import { useNavigate } from "react-router-dom";
+import InputSearch from "../../components/Admin/User/InputSearch";
+import UserModalCreate from "../../components/Admin/User/UserModalCreate";
+import UserModalUpdate from "../../components/Admin/User/UserModalUpdate";
+import UserViewDetail from "../../components/Admin/User/UserViewDetal";
+import UserExport from "../../components/Admin/User/data/UserExport";
+import UserImport from "../../components/Admin/User/data/UserImport";
+import { callDeleteUser, callFetchListUser } from "../../services/api";
+
+const UserList = () => {
+  const navigate = useNavigate();
+
+  const [listUser, setListUser] = useState([]);
+  const [current, setCurrent] = useState(1);
+  const [pageSize, setPageSize] = useState(2);
+  const [total, setTotal] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [filter, setFilter] = useState("");
+  const [sortQuery, setSortQuery] = useState("");
+  const [dataViewDetail, setDataViewDetail] = useState("");
+  const [openViewDetail, setOpenViewDetail] = useState(false);
+  const [openModalCreate, setOpenModalCreate] = useState(false);
+  const [openModalImport, setOpenModalImport] = useState(false);
+  const [openModalExport, setOpenModalExport] = useState(false);
+  const [openModalUpdate, setOpenModalUpdate] = useState(false);
+  const [dataUpdate, setDataUpdate] = useState("");
+
+  useEffect(() => {
+    fetchUser();
+  }, [current, pageSize, filter, sortQuery]);
+
+  // khi thay doi current va pageSize thi search died!
+  const fetchUser = async () => {
+    setIsLoading(true);
+    let query = `current=${current}&pageSize=${pageSize}`;
+    if (filter) {
+      query += `&${filter}`;
+    }
+
+    if (sortQuery) {
+      query += `&${sortQuery}`;
+    }
+
+    const res = await callFetchListUser(query);
+    if (res && res.data) {
+      setListUser(res.data.result);
+      setTotal(res.data.meta.total);
+    }
+
+    setIsLoading(false);
+  };
+
+  const handleDeleteUser = async (userId) => {
+    const res = await callDeleteUser(userId);
+    if (res && res.data) {
+      message.success("Xoá người dùng thành công!");
+      await fetchUser();
+    } else {
+      notification.error({
+        message: "Đã có lỗi xảy ra!",
+        description: res.message,
+      });
+    }
+  };
+
+  // sau này load động cột này -> cần có sự hợp tác của backend
+  const columns = [
+    {
+      title: "Code",
+      dataIndex: "_id",
+      width: 100,
+      fixed: "left",
+      render: (text, record, index) => {
+        return (
+          <a
+            href={`user#${record._id}`}
+            onClick={() => {
+              setDataViewDetail(record);
+              setOpenViewDetail(true);
+            }}
+          >
+            {record._id}
+          </a>
+        );
+      },
+    },
+    {
+      title: "Họ và tên",
+      dataIndex: "fullName",
+      sorter: true,
+      width: 100,
+      fixed: "left",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      width: 150,
+      sorter: true,
+    },
+    {
+      title: "Số điện thoại",
+      dataIndex: "phone",
+      width: 150,
+      sorter: true,
+    },
+    {
+      title: "Cập nhật ngày",
+      dataIndex: "updatedAt",
+      width: 150,
+      render: (text, record, index) => {
+        return (
+          <span>{moment(record.updatedAt).format("DD-MM-YYYY HH:mm:ss")}</span>
+        );
+      },
+      sorter: true,
+    },
+    {
+      title: "Thao tác",
+      width: 100,
+      fixed: "right",
+      render: (text, record, index) => {
+        return (
+          <>
+            <Popconfirm
+              placement="leftTop"
+              title={"Xác nhận xóa người dùng"}
+              description={"Bạn có chắc chắn muốn xóa người dùng này?"}
+              okText="Xác nhận"
+              cancelText="Hủy"
+              onConfirm={() => handleDeleteUser(record._id)}
+            >
+              <span>
+                <AiOutlineDelete
+                  style={{ color: "error", cursor: "pointer", marginRight: 10 }}
+                />
+              </span>
+            </Popconfirm>
+            <BsEye
+              style={{ cursor: "pointer", marginRight: 10 }}
+              onClick={(event) => {
+                // Điều hướng đến trang mới và truyền userId qua URL
+                navigate(`show/${record._id}`);
+              }}
+            />
+            <CiEdit
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setDataUpdate(record);
+                setOpenModalUpdate(true);
+              }}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  const renderHeader = () => (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <span style={{ fontWeight: "700", fontSize: "16" }}>
+        Danh sách người dùng
+      </span>
+      <span style={{ display: "flex", gap: 15 }}>
+        <Button
+          icon={<AiOutlineExport />}
+          type="primary"
+          onClick={() => setOpenModalExport(true)}
+        >
+          Export
+        </Button>
+        <Button
+          icon={<AiOutlineImport />}
+          type="primary"
+          onClick={() => setOpenModalImport(true)}
+        >
+          Import
+        </Button>
+        <Button
+          icon={<AiOutlinePlus />}
+          type="primary"
+          onClick={() => setOpenModalCreate(true)}
+        >
+          Thêm mới
+        </Button>
+        <Button
+          type="ghost"
+          onClick={() => {
+            setFilter("");
+            setSortQuery("");
+          }}
+        >
+          <AiOutlineReload />
+        </Button>
+      </span>
+    </div>
+  );
+
+  const handleSearch = (query) => {
+    setFilter(query);
+  };
+
+  const onChange = (pagination, filters, sorter, extra) => {
+    if (pagination && pagination.current !== current) {
+      setCurrent(pagination.current);
+    }
+
+    if (pagination && pagination.pageSize !== pageSize) {
+      setPageSize(pagination.pageSize);
+      setCurrent(1);
+    }
+
+    if (sorter && sorter.field) {
+      const q =
+        sorter.order === "ascend"
+          ? `sort=${sorter.field}`
+          : `sort=-${sorter.field}`;
+      setSortQuery(q);
+    }
+  };
+
+  return (
+    <>
+      <Row gutter={[20, 20]}>
+        <Col span={24}>
+          <InputSearch handleSearch={handleSearch} setFilter={setFilter} />
+        </Col>
+        <Col span={24}>
+          <Table
+            scroll={{
+              x: "100%",
+              y: 200,
+            }}
+            title={renderHeader}
+            bordered
+            loading={isLoading}
+            columns={columns}
+            dataSource={listUser}
+            onChange={onChange}
+            rowKey="_id"
+            pagination={{
+              current: current,
+              pageSize: pageSize,
+              showSizeChanger: true,
+              total: total,
+              showTotal: (total, range) => {
+                return (
+                  <div>
+                    {range[0]} - {range[1]} trên {total} dòng
+                  </div>
+                );
+              },
+            }}
+          />
+        </Col>
+      </Row>
+
+      <UserModalCreate
+        openModalCreate={openModalCreate}
+        setOpenModalCreate={setOpenModalCreate}
+        fetchUser={fetchUser}
+      />
+
+      <UserViewDetail
+        openViewDetail={openViewDetail}
+        setOpenViewDetail={setOpenViewDetail}
+        dataViewDetail={dataViewDetail}
+        setDataViewDetail={setDataViewDetail}
+      />
+
+      <UserImport
+        openModalImport={openModalImport}
+        setOpenModalImport={setOpenModalImport}
+        fetchUser={fetchUser}
+      />
+
+      <UserExport
+        openModalExport={openModalExport}
+        setOpenModalExport={setOpenModalExport}
+        listUser={listUser}
+      />
+
+      <UserModalUpdate
+        openModalUpdate={openModalUpdate}
+        setOpenModalUpdate={setOpenModalUpdate}
+        dataUpdate={dataUpdate}
+        setDataUpdate={setDataUpdate}
+        fetchUser={fetchUser}
+      />
+    </>
+  );
+};
+
+export default UserList;
