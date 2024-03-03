35c995d7db1606b6dd650a76c7524fc14211dec2
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-04 04:36:28 +0700

    create: thêm kiểu cho loại page, link đến page tương ứng trên button

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,10 +1,33 @@
+import { Button } from "antd";
 import React from "react";
+import { AiOutlineReload } from "react-icons/ai";
 import { IoArrowBack } from "react-icons/io5";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-const PageHeader = ({ title, numberBack }) => {
+const PageHeader = ({ title, numberBack, type }) => {
   // numberBack (-1): back 1 page, ...
   const navigate = useNavigate();
+  const user = useSelector((state) => state.user.user);
+
+  const isCreatePage = type === "create";
+  const isEditPage = type === "edit";
+
+  const currentPathname = window.location.pathname;
+
+  let editPathname;
+  if (currentPathname.includes("/show")) {
+    const showIndex = currentPathname.indexOf("/show");
+    const pathBeforeShow = currentPathname.substring(0, showIndex);
+    const userId = currentPathname.substring(showIndex + 6); // Length of "/show" is 5
+    editPathname = `${pathBeforeShow}/edit/${userId}`;
+  } else {
+    editPathname = currentPathname;
+  }
+
+  const handleView = () => {
+    navigate(editPathname);
+  };
 
   return (
     <div
@@ -21,6 +44,21 @@ const PageHeader = ({ title, numberBack }) => {
         onClick={() => navigate(numberBack)}
       />
       <span style={{ fontWeight: "700" }}>{title}</span>
+      <div
+        style={{ marginLeft: "auto", display: isCreatePage ? "none" : "flex" }}
+      >
+        <Button
+          style={{ marginRight: 10, display: isEditPage ? "none" : "flex" }}
+          type="primary"
+          onClick={() => handleView()}
+        >
+          Cập nhật
+        </Button>
+        <Button style={{ marginRight: 10 }}>Xoá</Button>
+        <Button>
+          <AiOutlineReload />
+        </Button>
+      </div>
     </div>
   );
 };
