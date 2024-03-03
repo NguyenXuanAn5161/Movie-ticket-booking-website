2128c74edf7eea1dcf56c84d0b18ca9241c29414
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-03 17:26:13 +0700

    fix: set bgc content layout admin

--- src/components/Admin/LayoutAdmin.jsx
+++ src/components/Admin/LayoutAdmin.jsx
@@ -198,7 +198,7 @@ const LayoutAdmin = () => {
             </a>
           </Dropdown>
         </div>
-        <Content style={{ margin: 10 }}>
+        <Content style={{ backgroundColor: "#F5F5F5", padding: 10 }}>
           {currentPath === "/admin" ? <Home /> : <CustomBreadcrumb />}
           <Outlet />
         </Content>
