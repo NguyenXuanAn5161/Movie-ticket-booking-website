19bf281917bf273db7ffd14ac817ea57c14e6245
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-03 22:23:10 +0700

    fix: css layout

--- src/components/Admin/LayoutAdmin.jsx
+++ src/components/Admin/LayoutAdmin.jsx
@@ -198,7 +198,12 @@ const LayoutAdmin = () => {
             </a>
           </Dropdown>
         </div>
-        <Content style={{ backgroundColor: "#F5F5F5", padding: 10 }}>
+        <Content
+          style={{
+            backgroundColor: "#F5F5F5",
+            padding: 10,
+          }}
+        >
           {currentPath === "/admin" ? <Home /> : <CustomBreadcrumb />}
           <Outlet />
         </Content>
