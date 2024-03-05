7386b744881ea747c994a39d2a46cbc2643afd77
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-05 17:02:13 +0700

    update: custom lại breadcumb cho từng crud tương ứng của mỗi menu cha

--- src/components/Breadcrumb/CustomBreadcrumb.jsx
+++ src/components/Breadcrumb/CustomBreadcrumb.jsx
@@ -1,5 +1,5 @@
 import { Breadcrumb } from "antd";
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function CustomBreadcrumb(props) {
@@ -9,52 +9,81 @@ function CustomBreadcrumb(props) {
     {
       path: "/admin",
       title: "",
+      key: "admin",
       children: [
         {
           path: "/dashboard",
           title: "Tổng quan",
+          key: "dashboard",
         },
         {
           path: "/order",
           title: "Hóa đơn",
+          key: "order",
         },
         {
           path: "/user",
           title: "Người dùng",
+          key: "user",
           children: [
             {
               path: "/show",
               title: "Xem chi tiết người dùng",
+              key: "userShow",
             },
             {
               path: "/create",
               title: "Tạo mới người dùng",
+              key: "userCreate",
             },
             {
               path: "/edit",
               title: "Cập nhật người dùng",
+              key: "userEdit",
             },
           ],
         },
         {
           path: "/movie",
           title: "Phim",
+          key: "movie",
+          children: [
+            {
+              path: "/show",
+              title: "Xem chi tiết phim",
+              key: "movieShow",
+            },
+            {
+              path: "/create",
+              title: "Tạo mới phim",
+              key: "movieCreate",
+            },
+            {
+              path: "/edit",
+              title: "Cập nhật phim",
+              key: "movieEdit",
+            },
+          ],
         },
         {
           path: "/cinema",
           title: "Rạp chiếu",
+          key: "cinema",
           children: [
             {
               path: "/room",
               title: "Phòng chiếu",
+              key: "room",
               children: [
                 {
                   path: "/seat",
                   title: "Ghế",
+                  key: "seat",
                 },
                 {
                   path: "/seatType",
                   title: "Loại ghế",
+                  key: "seatType",
                 },
               ],
             },
@@ -63,18 +92,27 @@ function CustomBreadcrumb(props) {
         {
           path: "/food",
           title: "Đồ ăn",
+          key: "food",
         },
         {
           path: "/foodCategories",
           title: "Loại đồ ăn",
+          key: "foodCategories",
         },
         {
           path: "/promotion",
           title: "Khuyến mãi",
+          key: "promotion",
         },
       ],
     },
   ];
+
+  let pathChildren = [];
+
+  useEffect(() => {
+    console.log("pathChildren useefect: ", pathChildren);
+  }, [pathChildren]);
 
   const getItemByPath = (path, itemList) => {
     for (let i = 0; i < itemList.length; i++) {
@@ -85,6 +123,9 @@ function CustomBreadcrumb(props) {
       if (item.children) {
         const foundItem = getItemByPath(path, item.children);
         if (foundItem) {
+          if (foundItem.children) {
+            pathChildren = foundItem.children;
+          }
           return foundItem;
         }
       }
@@ -105,6 +146,27 @@ function CustomBreadcrumb(props) {
 
     // Kiểm tra nếu route là 'show', 'edit', hoặc 'create' thì chỉ hiển thị span
     if (["/show", "/edit", "/create"].includes(route.path)) {
+      if (pathChildren.length > 0) {
+        console.log("pathChildren in itemRender: ", pathChildren);
+
+        // Kiểm tra xem route.path trùng với bất kỳ đường dẫn nào trong pathChildren
+        const matchingPathChild = pathChildren.find(
+          (pathChild) => pathChild.path === route.path
+        );
+
+        console.log(
+          "Check route.path cos trung hay khong: ",
+          matchingPathChild
+        );
+
+        // Nếu tìm thấy đường dẫn trùng khớp, trả về tiêu đề của mục đó
+        if (matchingPathChild) {
+          return (
+            <span>{matchingPathChild ? matchingPathChild.title : null}</span>
+          );
+        }
+      }
+      // Nếu không có đường dẫn trùng khớp hoặc không có pathChildren, hiển thị tiêu đề của item (nếu tồn tại)
       return <span>{item ? item.title : null}</span>;
     }
 
