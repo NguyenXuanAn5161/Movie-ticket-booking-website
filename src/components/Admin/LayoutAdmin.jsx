9704ae6f1ced00a472794217a5d3abee796e659e
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-07 12:14:01 +0700

    update: update Open current submenu only

--- src/components/Admin/LayoutAdmin.jsx
+++ src/components/Admin/LayoutAdmin.jsx
@@ -25,109 +25,84 @@ import "./layout.scss";
 
 const { Content, Footer, Sider } = Layout;
 
+// hàm tạo 1 mục menu
+const getItem = (label, key, icon, children, type) => {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+};
+
+// tạo các mục menu
 const items = [
-  {
-    label: <Link to="/admin/dashboard">Tổng quan</Link>,
-    key: "dashboard",
-    icon: <AppstoreOutlined />,
-  },
-  {
-    label: <Link to="/admin/order">Quản lý đơn hàng</Link>,
-    key: "order",
-    icon: <DollarCircleOutlined />,
-  },
-  {
-    label: <label>Quản lý người dùng</label>,
-    icon: <UserOutlined />,
-    key: "manageUser",
-    children: [
-      {
-        label: <Link to="/admin/user">Người dùng</Link>,
-        key: "user",
-        icon: <TeamOutlined />,
-      },
-      {
-        label: "Files1",
-        key: "file1",
-        icon: <TeamOutlined />,
-      },
-    ],
-  },
-  {
-    label: <label>Quản lý rạp phim</label>,
-    icon: <GiTheater />,
-    key: "manageTheater",
-    children: [
-      {
-        label: <Link to="/admin/cinema">Rạp phim</Link>,
-        key: "cinema",
-        icon: <GiTheater />,
-      },
-      {
-        label: <Link to="/admin/cinema/room">Phòng chiếu</Link>,
-        key: "room",
-        icon: <TbTheater />,
-      },
-      {
-        label: <Link to="/admin/cinema/room/seat">Ghế</Link>,
-        key: "seat",
-        icon: <MdEventSeat />,
-      },
-      {
-        label: <Link to="/admin/cinema/room/seatType">Loại ghế</Link>,
-        key: "seatType",
-        icon: <BiCategoryAlt />,
-      },
-    ],
-  },
-  {
-    label: <label>Quản lý phim</label>,
-    icon: <RiMovie2Line />,
-    key: "manageMovie",
-    children: [
-      {
-        label: <Link to="/admin/movie">Phim</Link>,
-        key: "movie",
-        icon: <RiMovie2Line />,
-      },
-      {
-        label: <label>Thể loại phim</label>,
-        key: "movieCategories",
-        icon: <BiCategoryAlt />,
-      },
-    ],
-  },
-  {
-    label: <label>Quản lý đồ ăn</label>,
-    icon: <IoFastFoodOutline />,
-    key: "manageFood",
-    children: [
-      {
-        label: <Link to="/admin/food">Đồ ăn</Link>,
-        key: "food",
-        icon: <IoFastFoodOutline />,
-      },
-      {
-        label: <Link to="/admin/foodCategories">Loại đồ ăn</Link>,
-        key: "foodCategories",
-        icon: <BiCategoryAlt />,
-      },
-    ],
-  },
-  {
-    label: <Link to="/admin/promotion">Khuyến mãi</Link>,
-    key: "promotion",
-    icon: <TbDiscount2 />,
-  },
+  getItem(
+    <Link to="/admin/dashboard">Tổng quan</Link>,
+    "dashboard",
+    <AppstoreOutlined />
+  ),
+  getItem(
+    <Link to="/admin/order">Quản lý đơn hàng</Link>,
+    "order",
+    <DollarCircleOutlined />
+  ),
+  getItem("Quản lý người dùng", "manageUser", <UserOutlined />, [
+    getItem(<Link to="/admin/user">Người dùng</Link>, "user", <TeamOutlined />),
+    getItem("Comming Soon", "commingsoon", <TeamOutlined />),
+  ]),
+  getItem("Quản lý rạp phim", "manageTheater", <GiTheater />, [
+    getItem(<Link to="/admin/cinema">Rạp phim</Link>, "cinema", <GiTheater />),
+    getItem(
+      <Link to="/admin/cinema/room">Phòng chiếu</Link>,
+      "room",
+      <TbTheater />
+    ),
+    getItem(
+      <Link to="/admin/cinema/room/seat">Ghế</Link>,
+      "seat",
+      <MdEventSeat />
+    ),
+    getItem(
+      <Link to="/admin/cinema/room/seatType">Loại ghế</Link>,
+      "seatType",
+      <BiCategoryAlt />
+    ),
+  ]),
+  getItem("Quản lý phim", "manageMovie", <RiMovie2Line />, [
+    getItem(<Link to="/admin/movie">Phim</Link>, "movie", <RiMovie2Line />),
+    getItem("Thể loại phim comming soon", "movieCategories", <BiCategoryAlt />),
+  ]),
+  getItem(<label>Quản lý đồ ăn</label>, "manageFood", <IoFastFoodOutline />, [
+    getItem(<Link to="/admin/food">Đồ ăn</Link>, "food", <IoFastFoodOutline />),
+    getItem(
+      <Link to="/admin/foodCategories">Loại đồ ăn</Link>,
+      "foodCategories",
+      <BiCategoryAlt />
+    ),
+  ]),
+  getItem(
+    <Link to="/admin/promotion">Khuyến mãi</Link>,
+    "promotion",
+    <TbDiscount2 />
+  ),
+];
+
+// Các keys của submenu cấp 1
+const rootSubmenuKeys = [
+  "manageUser",
+  "manageTheater",
+  "manageMovie",
+  "manageFood",
 ];
 
 const LayoutAdmin = () => {
+  const [openKeys, setOpenKeys] = useState([]); // State để lưu các keys của submenu đang mở
   const [collapsed, setCollapsed] = useState(false);
-  const [activeMenu, setActiveMenu] = useState("");
   const location = useLocation();
   const { pathname } = location;
   const currentPath = location.pathname;
-  const [openKeys, setOpenKeys] = useState([]);
   // lastIndexOf('a') lấy vị trí phần tử cuối cùng trong chuỗi
   // substring lấy chuỗi còn lại từ vị trí đó
   const lastSegment = pathname.substring(pathname.lastIndexOf("/") + 1);
@@ -137,15 +112,14 @@ const LayoutAdmin = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  // Hàm xử lý sự kiện khi một sub-menu được mở
-  const handleSubMenuOpen = (key) => {
-    setOpenKeys((prevOpenKeys) => {
-      if (prevOpenKeys.includes(key)) {
-        return prevOpenKeys.filter((k) => k !== key);
-      } else {
-        return [...prevOpenKeys, key];
-      }
-    });
+  // Hàm này xử lý sự kiện khi mở hoặc đóng một submenu
+  const onOpenChange = (keys) => {
+    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+    if (latestOpenKey && rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      setOpenKeys(keys); // Mở submenu nếu chưa được mở
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []); // Đóng các submenu khác nếu đã mở
+    }
   };
 
   const handleLogout = async () => {
@@ -186,72 +160,16 @@ const LayoutAdmin = () => {
         onCollapse={(value) => setCollapsed(value)}
       >
         <div style={{ height: 32, margin: 16, textAlign: "center" }}>Admin</div>
-        {/* <Menu
+        <Menu
           openKeys={openKeys}
           selectedKeys={[lastSegment]}
           mode="inline"
           items={items}
+          onOpenChange={onOpenChange}
           onClick={(e) => {
             setCollapsed(collapsed);
           }}
-        /> */}
-        <Menu
-          mode="inline"
-          openKeys={openKeys}
-          selectedKeys={[lastSegment]}
-          onClick={(e) => {
-            const { key } = e;
-            const subMenuKeys = items.map((item) => item.key);
-            if (!subMenuKeys.includes(key)) {
-              // Nếu không phải là sub-menu thì đóng toàn bộ các sub-menu
-              setOpenKeys([]);
-              // Kiểm tra xem key này thuộc subMenuKeys nào
-              items.forEach((item) => {
-                if (
-                  item.children &&
-                  item.children.some((child) => child.key === key)
-                ) {
-                  setOpenKeys([item.key]);
-                }
-              });
-            }
-          }}
-          onOpenChange={(keys) => {
-            // Xóa các key đã có trong mảng keys
-            const newOpenKeys = keys.filter((key) => !openKeys.includes(key));
-            // Thêm key hiện tại vào mảng mới
-            setOpenKeys(newOpenKeys);
-          }}
-        >
-          {items.map((item) => {
-            if (item.children) {
-              return (
-                <Menu.SubMenu
-                  key={item.key}
-                  title={
-                    <span>
-                      {item.icon}
-                      <span>{item.label}</span>
-                    </span>
-                  }
-                  onTitleClick={() => handleSubMenuOpen(item.key)}
-                >
-                  {item.children.map((child) => (
-                    <Menu.Item key={child.key} icon={child.icon}>
-                      {child.label}
-                    </Menu.Item>
-                  ))}
-                </Menu.SubMenu>
-              );
-            } else {
-              return (
-                <Menu.Item key={item.key} icon={item.icon}>
-                  {item.label}
-                </Menu.Item>
-              );
-            }
-          })}
-        </Menu>
+        />
       </Sider>
       <Layout style={{ backgroundColor: "white" }}>
         <div className="admin-header">
