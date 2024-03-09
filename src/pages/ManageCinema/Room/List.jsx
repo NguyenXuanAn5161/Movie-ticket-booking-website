89d055e1a1a12e48a832a64f16a53849d8876926
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-09 10:24:56 +0700

    create: xem và tạo mới phòng chiếu

---
+++ src/pages/ManageCinema/Room/List.jsx
@@ -0,0 +1,282 @@
+import {
+  Button,
+  Col,
+  Popconfirm,
+  Row,
+  Table,
+  message,
+  notification,
+} from "antd";
+import moment from "moment";
+import { useEffect, useState } from "react";
+import {
+  AiOutlineDelete,
+  AiOutlineExport,
+  AiOutlineImport,
+  AiOutlinePlus,
+  AiOutlineReload,
+} from "react-icons/ai";
+import { BsEye } from "react-icons/bs";
+import { CiEdit } from "react-icons/ci";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import InputSearch from "../../../components/InputSearch/InputSearch";
+
+const RoomList = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  // mặc định #2
+  const [listData, setListData] = useState([]);
+  const [current, setCurrent] = useState(1);
+  const [pageSize, setPageSize] = useState(2);
+  const [total, setTotal] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [filter, setFilter] = useState("");
+  const [sortQuery, setSortQuery] = useState("sort=-updatedAt"); // default sort by updateAt mới nhất
+  const [openModalImport, setOpenModalImport] = useState(false);
+  const [openModalExport, setOpenModalExport] = useState(false);
+
+  // mặc định #2
+  useEffect(() => {
+    fetchData();
+  }, [current, pageSize, filter, sortQuery]);
+
+  // khi thay doi current va pageSize thi search died!
+  // mặc định #2
+  const fetchData = async () => {
+    setIsLoading(true);
+    let query = `current=${current}&pageSize=${pageSize}`;
+    if (filter) {
+      query += `&${filter}`;
+    }
+
+    if (sortQuery) {
+      query += `&${sortQuery}`;
+    }
+
+    // thay đổi #1 api call
+    const res = await callFetchListUser(query);
+    if (res && res.data) {
+      setListData(res.data.result);
+      setTotal(res.data.meta.total);
+    }
+
+    setIsLoading(false);
+  };
+
+  // mặc định #2
+  const handleDeleteData = async (dataId) => {
+    // thay đổi #1 api call
+    const res = await callDeleteUser(dataId);
+    if (res && res.data) {
+      // thay đổi #1 message
+      message.success("Xoá phòng thành công!");
+      await fetchData();
+    } else {
+      notification.error({
+        message: "Đã có lỗi xảy ra!",
+        description: res.message,
+      });
+    }
+  };
+
+  const handleView = (data, url) => {
+    // thay đổi #1
+    dispatch(doSetMovie(data));
+    navigate(`${url}/${data._id}`);
+  };
+
+  // thay đổi #1
+  const columns = [
+    {
+      title: "Mã phòng",
+      dataIndex: "code",
+      width: 100,
+      fixed: "left",
+    },
+    {
+      title: "Tên phòng",
+      dataIndex: "name",
+      sorter: true,
+      width: 100,
+      fixed: "left",
+    },
+    {
+      title: "Loại phòng",
+      dataIndex: "typeRoom",
+      width: 150,
+      sorter: true,
+    },
+    {
+      title: "Tổng số ghế",
+      dataIndex: "totalSeat",
+      width: 150,
+      sorter: true,
+    },
+    {
+      title: "Cập nhật ngày",
+      dataIndex: "updatedAt",
+      width: 150,
+      render: (text, record, index) => {
+        return (
+          <span>{moment(record.updatedAt).format("DD-MM-YYYY HH:mm:ss")}</span>
+        );
+      },
+      sorter: true,
+    },
+    {
+      title: "Thao tác",
+      width: 100,
+      fixed: "right",
+      render: (text, record, index) => {
+        return (
+          <>
+            <Popconfirm
+              placement="leftTop"
+              // thay đổi #1 sửa title và description
+              title={"Xác nhận xóa phòng chiếu"}
+              description={"Bạn có chắc chắn muốn xóa phòng chiếu này?"}
+              okText="Xác nhận"
+              cancelText="Hủy"
+              onConfirm={() => handleDeleteData(record._id)}
+            >
+              <span>
+                <AiOutlineDelete
+                  style={{ color: "red", cursor: "pointer", marginRight: 10 }}
+                />
+              </span>
+            </Popconfirm>
+            <BsEye
+              style={{ cursor: "pointer", marginRight: 10 }}
+              onClick={() => handleView(record, "show")}
+            />
+            <CiEdit
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                handleView(record, "edit");
+              }}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  const renderHeader = () => (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      {/* thay đổi #1 */}
+      <span style={{ fontWeight: "700", fontSize: "16" }}>
+        Danh sách phòng chiếu phim
+      </span>
+      <span style={{ display: "flex", gap: 15 }}>
+        <Button
+          icon={<AiOutlineExport />}
+          type="primary"
+          onClick={() => setOpenModalExport(true)}
+        >
+          Export
+        </Button>
+        <Button
+          icon={<AiOutlineImport />}
+          type="primary"
+          onClick={() => setOpenModalImport(true)}
+        >
+          Import
+        </Button>
+        <Button
+          icon={<AiOutlinePlus />}
+          type="primary"
+          onClick={(event) => {
+            // Điều hướng đến trang mới và truyền userId qua URL
+            navigate(`create`);
+          }}
+        >
+          Thêm mới
+        </Button>
+        <Button
+          type="ghost"
+          onClick={() => {
+            setFilter("");
+            setSortQuery("");
+          }}
+        >
+          <AiOutlineReload />
+        </Button>
+      </span>
+    </div>
+  );
+
+  // mặc định #2
+  const handleSearch = (query) => {
+    setFilter(query);
+  };
+
+  // mặc định #2
+  const onChange = (pagination, filters, sorter, extra) => {
+    if (pagination && pagination.current !== current) {
+      setCurrent(pagination.current);
+    }
+
+    if (pagination && pagination.pageSize !== pageSize) {
+      setPageSize(pagination.pageSize);
+      setCurrent(1);
+    }
+
+    if (sorter && sorter.field) {
+      const q =
+        sorter.order === "ascend"
+          ? `sort=${sorter.field}`
+          : `sort=-${sorter.field}`;
+      setSortQuery(q);
+    }
+  };
+
+  // thay đổi #1
+  const itemSearch = [
+    { field: "code", label: "Mã phòng" },
+    { field: "name", label: "Tên phòng" },
+    { field: "typeRoom", label: "Loại phòng" },
+  ];
+
+  return (
+    <>
+      <Row gutter={[20, 20]}>
+        <Col span={24}>
+          <InputSearch itemSearch={itemSearch} handleSearch={handleSearch} />
+        </Col>
+        <Col span={24}>
+          <Table
+            scroll={{
+              x: "100%",
+              y: 200,
+            }}
+            title={renderHeader}
+            bordered
+            // thay đổi #1
+            // loading={isLoading}
+            columns={columns}
+            dataSource={listData}
+            onChange={onChange}
+            // thay đổi #1
+            rowKey="code"
+            pagination={{
+              current: current,
+              pageSize: pageSize,
+              showSizeChanger: true,
+              total: total,
+              showTotal: (total, range) => {
+                return (
+                  <div>
+                    {range[0]} - {range[1]} trên {total} dòng
+                  </div>
+                );
+              },
+            }}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default RoomList;
