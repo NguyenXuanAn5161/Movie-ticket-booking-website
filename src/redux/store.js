221b3b82adb6a0257544b37a3da810aa8a739c12
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-09 10:29:54 +0700

    create: tạo mới store cho promotion, movie, movieGenre

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "../redux/account/accountSlice";
 import userReducer from "../redux/account/userSlice";
+import promotionReducer from "./account/promotionSlice";
+import movieGenreReducer from "./movie/movieGenreSlice";
+import movieReducer from "./movie/movieSlice";
 
 export const store = configureStore({
   reducer: {
     account: accountReducer,
     user: userReducer,
+    movie: movieReducer,
+    movieGenre: movieGenreReducer,
+    promotion: promotionReducer,
   },
 });
