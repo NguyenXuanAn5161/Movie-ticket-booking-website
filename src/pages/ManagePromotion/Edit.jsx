77612162ba1f852dd62dc62c4b5ee88860d51773
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-11 00:12:30 +0700

    update: form crud promotionline

--- src/pages/ManagePromotion/Edit.jsx
+++ src/pages/ManagePromotion/Edit.jsx
@@ -1,5 +1,176 @@
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Input,
+  Radio,
+  Row,
+  message,
+  notification,
+} from "antd";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import PageHeader from "../../components/PageHeader/PageHeader";
+import { callUpdateUser } from "../../services/api";
+
 const PromotionEdit = () => {
-  return <>Edit promoton header</>;
+  // thay đổi #1
+  const promotionHeader = useSelector((state) => state.promotion.promotion);
+  // mặc định #2
+  const navigate = useNavigate();
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    form.resetFields();
+    // thay đổi #1 [], setfields
+    form.setFieldsValue(promotionHeader); // Cập nhật dữ liệu vào form
+  }, [promotionHeader, form]);
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setUserData({ ...userData, [name]: value });
+  // };
+
+  const onFinish = async (values) => {
+    console.log("value check: ", values);
+    // thay đổi #1
+    const { _id, fullName, phone } = values;
+    setIsSubmit(true);
+    // thay đổi #1 api call
+    const res = await callUpdateUser(_id, fullName, phone);
+    if (res && res.data) {
+      // thay đổi #1 message và url
+      message.success("Cập nhật khuyến mãi thành công thành công!");
+      navigate("/admin/promotion");
+    } else {
+      notification.error({
+        message: "Đã có lỗi xảy ra!",
+        description: res.message,
+      });
+    }
+    setIsSubmit(false);
+  };
+
+  return (
+    <>
+      <PageHeader
+        title="Cập nhật thông tin khuyến mãi"
+        numberBack={-1}
+        type="edit"
+      />
+      <Card bordered={false}>
+        <Form form={form} onFinish={onFinish}>
+          <Row gutter={[16]}>
+            <Col span={6}>
+              <Form.Item
+                labelCol={{ span: 24 }}
+                label="Mã khuyến mãi"
+                name="code"
+                rules={[
+                  {
+                    required: true,
+                    message: "Vui lòng nhập mã khuyến mãi!",
+                  },
+                ]}
+              >
+                <Input placeholder="Nhập mã khuyến mãi" />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                labelCol={{ span: 24 }}
+                label="Tên khuyến mãi"
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: "Vui lòng nhập tên khuyến mãi!",
+                  },
+                ]}
+              >
+                <Input placeholder="Nhập tên khuyến mãi" />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item
+                labelCol={{ span: 24 }}
+                label="Trạng thái"
+                name="status"
+                rules={[
+                  {
+                    required: true,
+                    message: "Vui lòng chọn trạng thái!",
+                  },
+                ]}
+              >
+                <Radio.Group value={promotionHeader?.status}>
+                  <Radio.Button value="available">Hoạt động</Radio.Button>
+                  <Radio.Button value="unavailable">
+                    Ngưng hoạt động
+                  </Radio.Button>
+                </Radio.Group>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                labelCol={{ span: 24 }}
+                label="Ngày bắt đầu"
+                name="start_date"
+                rules={[
+                  {
+                    required: true,
+                    message: "Vui lòng chọn ngày bắt đầu!",
+                  },
+                ]}
+              >
+                <Input type="date" style={{ width: "100%" }} />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                labelCol={{ span: 24 }}
+                label="Ngày kết thúc"
+                name="end_date"
+                rules={[
+                  {
+                    required: true,
+                    message: "Vui lòng chọn ngày kết thúc!",
+                  },
+                ]}
+              >
+                <Input type="date" style={{ width: "100%" }} />
+              </Form.Item>
+            </Col>
+            <Col span={24}>
+              <Form.Item
+                labelCol={{ span: 24 }}
+                name="description"
+                label="Mô Tả"
+                rules={[
+                  {
+                    required: true,
+                    message: "Vui lòng nhập mô tả!",
+                  },
+                ]}
+              >
+                <Input.TextArea placeholder="Nhập mô tả" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Form.Item>
+              <Button loading={isSubmit} type="primary" htmlType="submit">
+                Cập nhật
+              </Button>
+            </Form.Item>
+          </Row>
+        </Form>
+      </Card>
+    </>
+  );
 };
 
 export default PromotionEdit;
