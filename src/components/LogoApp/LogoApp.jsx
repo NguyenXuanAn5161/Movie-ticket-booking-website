6e519391e3d6e55a904e22b99f3c56a3f2aa99e6
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-25 09:31:42 +0700

    fix: fix warning Invalid DOM property `clip-rule`

--- src/components/LogoApp/LogoApp.jsx
+++ src/components/LogoApp/LogoApp.jsx
@@ -10,9 +10,9 @@ const LogoApp = ({ ...props }) => {
       >
         <path
           fill="#FE4A4B"
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M0 4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v1.382l2.553-1.276A1 1 0 0 1 16 5v6a1 1 0 0 1-1.447.894L12 10.618V12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4 2.5a.5.5 0 0 1 .724-.447l3 1.5a.5.5 0 0 1 0 .894l-3 1.5A.5.5 0 0 1 4 9.5z"
-          clip-rule="evenodd"
+          clipRule="evenodd"
         />
       </svg>
     </div>
