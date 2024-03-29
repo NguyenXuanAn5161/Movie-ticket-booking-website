a274f55652d46a356e6e8853b813481dfb537c2f
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-29 11:02:05 +0700

    create: basic home tối thiểu

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,31 @@
-const Home = () => {
-  return <>Home</>;
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AdminHome = () => {
+  return (
+    <div>
+      <h1>Trang quản trị viên</h1>
+      <p>Chào mừng đến với hệ thống quản trị viên!</p>
+      <p>
+        Bạn có thể quản lý các bộ phim, lịch chiếu, vé xem phim và người dùng.
+      </p>
+      <ul>
+        <li>
+          <Link to="/admin/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/admin/order">Quản lý đơn đặt hàng</Link>
+        </li>
+        <li>
+          <Link to="/admin/user">Quản lý người dùng</Link>
+        </li>
+        <li>
+          <Link to="/admin/movie">Quản lý phim</Link>
+        </li>
+        {/* Thêm các liên kết khác ở đây tương ứng với các trang bạn muốn */}
+      </ul>
+    </div>
+  );
 };
 
-export default Home;
+export default AdminHome;
