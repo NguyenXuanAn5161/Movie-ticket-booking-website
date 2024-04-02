f43e01f2e426b59b7a7b2505d6990fdd3b476c7c
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-04-02 09:49:06 +0700

    update: call api get all and search

--- src/pages/ManageMovie/Movie/List.jsx
+++ src/pages/ManageMovie/Movie/List.jsx
@@ -21,7 +21,10 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import InputSearch from "../../../components/InputSearch/InputSearch";
 import { doSetMovie } from "../../../redux/movie/movieSlice";
-import { callFetchListUser } from "../../../services/api";
+import {
+  callDeleteGenreMovie,
+  callFetchListMovie,
+} from "../../../services/apiMovie";
 
 const MovieList = () => {
   const data = [
@@ -166,7 +169,7 @@ const MovieList = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   // mặc định #2
-  const [listData, setListData] = useState(data);
+  const [listData, setListData] = useState([]);
   const [current, setCurrent] = useState(1);
   const [pageSize, setPageSize] = useState(2);
   const [total, setTotal] = useState(0);
@@ -184,7 +187,7 @@ const MovieList = () => {
   // mặc định #2
   const fetchData = async () => {
     setIsLoading(true);
-    let query = `current=${current}&pageSize=${pageSize}`;
+    let query = `page=${current - 1}&size=${pageSize}`;
     if (filter) {
       query += `&${filter}`;
     }
@@ -194,10 +197,11 @@ const MovieList = () => {
     }
 
     // thay đổi #1 api call
-    const res = await callFetchListUser(query);
-    if (res && res.data) {
-      // setListData(res.data.result);
-      // setTotal(res.data.meta.total);
+    const res = await callFetchListMovie(query);
+    console.log("res", res);
+    if (res?.content) {
+      setListData(res.content);
+      setTotal(res.totalElements);
     }
 
     setIsLoading(false);
@@ -206,8 +210,8 @@ const MovieList = () => {
   // mặc định #2
   const handleDeleteData = async (dataId) => {
     // thay đổi #1 api call
-    const res = await callDeleteUser(dataId);
-    if (res && res.data) {
+    const res = await callDeleteGenreMovie(dataId);
+    if (res?.status === 200) {
       // thay đổi #1 message
       message.success("Xoá phim thành công!");
       await fetchData();
@@ -283,7 +287,7 @@ const MovieList = () => {
               description={"Bạn có chắc chắn muốn xóa phim này?"}
               okText="Xác nhận"
               cancelText="Hủy"
-              onConfirm={() => handleDeleteData(record._id)}
+              onConfirm={() => handleDeleteData(record.id)}
             >
               <span>
                 <AiOutlineDelete
@@ -344,7 +348,17 @@ const MovieList = () => {
 
   // mặc định #2
   const handleSearch = (query) => {
-    setFilter(query);
+    let q = "";
+    for (const key in query) {
+      if (query.hasOwnProperty(key)) {
+        const label = key;
+        const value = query[key];
+        if (value) {
+          q += `&${label}=${value}`;
+        }
+      }
+    }
+    setFilter(q);
   };
 
   // mặc định #2
@@ -369,9 +383,9 @@ const MovieList = () => {
 
   // thay đổi #1
   const itemSearch = [
-    { field: "movieName", label: "Tên phim" },
-    { field: "author", label: "Tác giả" },
-    { field: "movieCategories", label: "Thể loại" },
+    { field: "name", label: "Tên phim" },
+    { field: "genreId", label: "Thể loại" },
+    { field: "cinemaId", label: "Rạp" },
   ];
 
   return (
