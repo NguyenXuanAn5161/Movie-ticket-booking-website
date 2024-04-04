be087ce0356585df37e272bae6940c0506cf8561
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-04-04 14:44:23 +0700

    update: f5, call api promotion line

--- src/pages/ManagePromotion/Show.jsx
+++ src/pages/ManagePromotion/Show.jsx
@@ -8,7 +8,7 @@ import {
   Tag,
 } from "antd";
 import moment from "moment";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   AiOutlineDelete,
   AiOutlineExport,
@@ -17,12 +17,38 @@ import {
 } from "react-icons/ai";
 import { BsEye } from "react-icons/bs";
 import { CiEdit } from "react-icons/ci";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import PageHeader from "../../components/PageHeader/PageHeader";
+import { doSetPromotion } from "../../redux/promotion/promotionSlice";
+import {
+  callGetPromotionHeaderById,
+  callGetPromotionLineByPromotionId,
+} from "../../services/apiMovie";
 import PromotionLineModalForm from "./PromotionLines/PromotionLineForm";
 
 const PromotionShow = () => {
   const promotionHeader = useSelector((state) => state.promotion.promotion);
+  const { promotionId } = useParams();
+  const dispatch = useDispatch();
+  // f5 fetch data
+  useEffect(() => {
+    if (!promotionHeader) {
+      getPromotionHeaderById();
+    }
+  }, [promotionHeader]);
+
+  const getPromotionHeaderById = async () => {
+    const res = await callGetPromotionHeaderById(promotionId);
+    if (res) {
+      dispatch(doSetPromotion(res));
+    } else {
+      notification.error({
+        message: "Đã có lỗi xảy ra!",
+        description: res.response.data.message,
+      });
+    }
+  };
 
   const [isLoading, setIsLoading] = useState(false);
   const [openModalCreate, setOpenModalCreate] = useState(false);
@@ -33,24 +59,45 @@ const PromotionShow = () => {
   const [dataUpdate, setDataUpdate] = useState(null);
   const [current, setCurrent] = useState(1);
   const [pageSize, setPageSize] = useState(2);
-  const [total, setTotal] = useState(promotionHeader?.promotionLines.length);
+  const [total, setTotal] = useState(0);
   const [filter, setFilter] = useState(null);
   const [sortQuery, setSortQuery] = useState("");
+  const [promotionLines, setPromotionLines] = useState([]);
 
-  // Hàm thay đổi trang
-  const onChange = (pagination, filters, sorter) => {
-    console.log("params", pagination, filters, sorter);
-    setCurrent(pagination.current);
-    setPageSize(pagination.pageSize);
+  // get promotion lines
+  useEffect(() => {
+    getPromotionLines();
+  }, [current, pageSize, filter, sortQuery]);
+
+  // khi thay doi current va pageSize thi search died!
+  // mặc định #2
+  const getPromotionLines = async () => {
+    setIsLoading(true);
+    let query = `page=${
+      current - 1
+    }&size=${pageSize}&promotionId=${promotionId}`;
+
+    if (filter) {
+      query += `&${filter}`;
+    }
+
+    // if (sortQuery) {
+    //   query += `&${sortQuery}`;
+    // }
+
+    // thay đổi #1 api call
+    const res = await callGetPromotionLineByPromotionId(query);
+    console.log("res", res);
+    if (res?.content) {
+      setPromotionLines(res.content);
+      setTotal(res.totalElements);
+    }
+
+    setIsLoading(false);
   };
 
   // render thông tin promotion header
   const items = [
-    {
-      label: "Mã khuyến mãi",
-      key: "1",
-      children: promotionHeader?.code,
-    },
     {
       label: "Tên khuyến mãi",
       key: "2",
@@ -59,23 +106,21 @@ const PromotionShow = () => {
     {
       label: "Ngày bắt đầu",
       key: "3",
-      children: moment(promotionHeader?.start_date).format(
+      children: moment(promotionHeader?.startDate).format(
         "DD-MM-YYYY HH:mm:ss"
       ),
     },
     {
       label: "Ngày kết thúc",
       key: "4",
-      children: moment(promotionHeader?.end_date).format("DD-MM-YYYY HH:mm:ss"),
+      children: moment(promotionHeader?.endDate).format("DD-MM-YYYY HH:mm:ss"),
     },
     {
       label: "Trạng thái",
       key: "5",
       children: (
-        <Tag color={promotionHeader?.status === "available" ? "green" : "red"}>
-          {promotionHeader?.status === "available"
-            ? "Hoạt động"
-            : "Không hoạt động"}
+        <Tag color={promotionHeader?.status ? "success" : "error"}>
+          {promotionHeader?.status ? "Hoạt động" : "Không hoạt động"}
         </Tag>
       ),
     },
@@ -87,7 +132,7 @@ const PromotionShow = () => {
         xs: 1,
         sm: 2,
         md: 2,
-        lg: 5,
+        lg: 4,
       },
     },
   ];
@@ -143,18 +188,22 @@ const PromotionShow = () => {
     },
     {
       title: "Ngày bắt đầu",
-      dataIndex: "start_date",
+      dataIndex: "startDate",
       width: 120,
       render: (text, record, index) => {
-        return <span>{moment(record.start_date).format("DD-MM-YYYY")}</span>;
+        return (
+          <span>{moment(record.startDate).format("DD-MM-YYYY HH:mm:ss")}</span>
+        );
       },
     },
     {
       title: "Ngày kết thúc",
-      dataIndex: "end_date",
+      dataIndex: "endDate",
       width: 130,
       render: (text, record, index) => {
-        return <span>{moment(record.end_date).format("DD-MM-YYYY")}</span>;
+        return (
+          <span>{moment(record.endDate).format("DD-MM-YYYY HH:mm:ss")}</span>
+        );
       },
     },
     {
@@ -169,8 +218,8 @@ const PromotionShow = () => {
       key: "status",
       width: 150,
       render: (status) => (
-        <Tag color={status === "available" ? "green" : "red"}>
-          {status === "available" ? "Hoạt động" : "Không hoạt động"}
+        <Tag color={status ? "success" : "error"}>
+          {status ? "Hoạt động" : "Không hoạt động"}
         </Tag>
       ),
     },
@@ -241,9 +290,9 @@ const PromotionShow = () => {
               xs: 1,
               sm: 2,
               md: 2,
-              lg: 5,
-              xl: 5,
-              xxl: 5,
+              lg: 4,
+              xl: 4,
+              xxl: 4,
             }}
             items={items}
           />
@@ -258,8 +307,7 @@ const PromotionShow = () => {
             bordered
             loading={isLoading}
             columns={columns}
-            dataSource={promotionHeader?.promotionLines}
-            onChange={onChange}
+            dataSource={promotionLines}
             rowKey="id" // Đảm bảo id là trường định danh duy nhất của mỗi chương trình khuyến mãi
             pagination={{
               current: current,
@@ -283,7 +331,11 @@ const PromotionShow = () => {
           openModalCreate ? "create" : openModalUpdate ? "update" : "view"
         }
         data={
-          openModalCreate ? null : openModalUpdate ? dataUpdate : dataViewDetail
+          openModalCreate
+            ? promotionId
+            : openModalUpdate
+            ? dataUpdate
+            : dataViewDetail
         }
         setData={
           openModalCreate
