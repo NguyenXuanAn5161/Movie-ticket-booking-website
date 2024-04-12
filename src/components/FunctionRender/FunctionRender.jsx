fa603d0230bc30874b7963a130547e4b7aaf701d
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-04-12 16:15:43 +0700

    update: status, typePrice, priceName

--- src/components/FunctionRender/FunctionRender.jsx
+++ src/components/FunctionRender/FunctionRender.jsx
@@ -3,6 +3,36 @@ import moment from "moment";
 import React from "react";
 import { FORMAT_DATE_HH_MM_SS } from "../../utils/constant";
 
+const getStatusMap = (type) => {
+  switch (type) {
+    case "movie":
+      return {
+        true: { color: "success", label: "Được chiếu" },
+        false: { color: "error", label: "Ngừng chiếu" },
+      };
+    case "food":
+      return {
+        true: { color: "success", label: "Có sẵn" },
+        false: { color: "error", label: "Không có sẵn" },
+      };
+    default:
+      return {
+        true: { color: "success", label: "Hoạt động" },
+        false: { color: "error", label: "Không hoạt động" },
+      };
+  }
+};
+
+export const renderStatus = (type) => (text, record) => {
+  const statusMap = getStatusMap(type);
+  const statusKey = String(record.status);
+  const { color, label } = statusMap[statusKey] || {
+    color: "default",
+    label: "Không xác định",
+  };
+  return <Tag color={color}>{label}</Tag>;
+};
+
 export const renderAddress = (text, record) => {
   return (
     <span>
@@ -16,21 +46,43 @@ export const renderDate = (text, record) => {
   return <span>{moment(text).format(FORMAT_DATE_HH_MM_SS)}</span>;
 };
 
-export const renderStatus = (text, record) => {
-  return (
-    <Tag color={record.status ? "success" : "error"}>
-      {record.status ? "Được chiếu" : "Ngừng chiếu"}
-    </Tag>
-  );
-};
-
 export const renderCurrency = (text, record) => {
   return (
     <span>
-      {new Intl.NumberFormat("vi-VN", {
-        style: "currency",
-        currency: "VND",
-      }).format(text ?? 0)}
+      {text !== 0 && text !== null ? (
+        new Intl.NumberFormat("vi-VN", {
+          style: "currency",
+          currency: "VND",
+        }).format(text ?? 0)
+      ) : (
+        <span style={{ color: "red", fontWeight: "700" }}>Chưa tạo giá</span>
+      )}
+    </span>
+  );
+};
+
+export const renderTypePrice = (text, record) => {
+  return (
+    <span>
+      {record?.type === "TYPE_SEAT"
+        ? "Loại ghế"
+        : record?.type === "FOOD"
+        ? "Đồ ăn"
+        : "Phòng"}
+    </span>
+  );
+};
+
+export const renderPriceName = (text, record) => {
+  return (
+    <span>
+      {record.type === "TYPE_SEAT"
+        ? record?.name === "SWEETBOX"
+          ? "Ghế đôi"
+          : record?.name === "STANDARD"
+          ? "Ghế thường"
+          : "Ghế vip"
+        : record?.name}
     </span>
   );
 };
