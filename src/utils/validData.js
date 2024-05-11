97ff92a6cdd9077f47f62855a4062f22fb02b7fb
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-05-11 22:10:43 +0700

    validator

---
+++ src/utils/validData.js
@@ -0,0 +1,37 @@
+const validateField = (fieldName, regex, invalidMessage) => {
+  return (rule, value, callback) => {
+    if (!value) {
+      callback();
+    } else if (regex && /\s/.test(value)) {
+      callback(`${fieldName} không được chứa khoảng trắng!`);
+    } else if (regex && !regex.test(value)) {
+      callback(invalidMessage || `${fieldName} không hợp lệ!`);
+    } else {
+      callback();
+    }
+  };
+};
+
+export const validateUsername = validateField(
+  "Họ và tên",
+  /^[a-zA-Z ]+$/,
+  "Họ và tên chỉ chứa bảng chữ cái tiếng anh và dấu cách!"
+);
+
+export const validatePhoneNumber = validateField(
+  "Số điện thoại",
+  /^(03|07|08|09|05)[0-9]{8}$/,
+  "Số điện thoại phải đủ 10 số và bắt đầu bằng các đầu số sau: 03x, 07x, 08x, 09x hoặc 05x."
+);
+
+export const validateEmail = validateField(
+  "Email",
+  /^[^\s@]+@(gmail\.com|yahoo\.com|outlook\.com)$/,
+  "Email chỉ chứa các miền @gmail.com/@yahoo.com/outlook.com!"
+);
+
+export const validatePassword = validateField(
+  "Mật khẩu",
+  /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/,
+  "Mật khẩu phải chứa ít nhất 1 ký tự hoa, 1 chữ số, và có ít nhất 6 ký tự!"
+);
