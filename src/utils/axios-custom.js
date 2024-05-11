26c6ccbf632996e019c59effd1c84daa4f0bc05c
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-05-11 22:34:59 +0700

    truyền token

--- src/utils/axios-custom.js
+++ src/utils/axios-custom.js
@@ -10,6 +10,7 @@ const api = axios.create({
   //   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
   },
 });
 
