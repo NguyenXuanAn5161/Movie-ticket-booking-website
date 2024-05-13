354dfa382f1887863a2f93054e1fae1b6aa6e41b
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-05-13 11:42:35 +0700

    xong phần list show time

--- src/pages/ManageMovie/Schedule/List.jsx
+++ src/pages/ManageMovie/Schedule/List.jsx
@@ -1,22 +1,29 @@
 import { Col, Row, Table, message, notification } from "antd";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import ActionButtons from "../../../components/Button/ActionButtons";
 import {
   renderDate,
   renderStatus,
 } from "../../../components/FunctionRender/FunctionRender";
+import SearchList from "../../../components/InputSearch/SearchList";
 import TableHeader from "../../../components/TableHeader/TableHeader";
 import { doSetSchedule } from "../../../redux/schedule/scheduleSlice";
 import { callFetchListCinema } from "../../../services/apiCinema";
 import { callFetchListMovie } from "../../../services/apiMovie";
 import { callFetchListShowtime } from "../../../services/apiShowTime";
+import { FORMAT_DATE_SEND_SERVER } from "../../../utils/constant";
 import { createColumn } from "../../../utils/createColumn";
 
 const ScheduleList = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.account.user);
+  const userRoles = user?.roles;
+  const checked = userRoles?.some((role) => role === "ROLE_ADMIN");
+
   // mặc định #2
   const [listData, setListData] = useState([]);
   const [current, setCurrent] = useState(1);
@@ -27,6 +34,36 @@ const ScheduleList = () => {
   const [sortQuery, setSortQuery] = useState("sort=-updatedAt"); // default sort by updateAt mới nhất
   const [listCinema, setListCinema] = useState([]);
   const [listMovie, setListMovie] = useState([]);
+  const [selectedCinema, setSelectedCinema] = useState(null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const handleSelectedMovie = () => {
+    if (filter.includes("movieId")) {
+      const movieId = filter.split("=")[1].split("&")[0];
+      const movie = listMovie.find((item) => item.value == movieId);
+      setSelectedMovie(movie?.label);
+    } else {
+      const movieId1 = listMovie.find((item) => item.value === 1);
+      setSelectedMovie(movieId1?.label);
+    }
+  };
+
+  const handleSelectedCinema = () => {
+    if (filter.includes("cinemaId")) {
+      const cinemaId = filter.split("=")[1].split("&")[0];
+      const cinema = listCinema.find((item) => item.value == cinemaId);
+      console.log("cinema", cinema);
+      setSelectedCinema(cinema?.label);
+    } else {
+      const cinemaId1 = listCinema.find((item) => item.value === 1);
+      setSelectedCinema(cinemaId1?.label);
+    }
+  };
+
+  useEffect(() => {
+    handleSelectedCinema();
+    handleSelectedMovie();
+  }, [filter]);
 
   useEffect(() => {
     fetchListCinema();
@@ -41,6 +78,9 @@ const ScheduleList = () => {
         value: item.id,
       }));
       setListCinema(cinemas);
+      // lấy ra rạp có id là 1
+      const cinemaId1 = cinemas.find((item) => item.value === 1);
+      setSelectedCinema(cinemaId1.label);
     }
   };
 
@@ -57,6 +97,9 @@ const ScheduleList = () => {
         value: item.id,
       }));
       setListMovie(movies);
+      // lấy ra movie có id là 1
+      const movieId1 = movies.find((item) => item.value === 1);
+      setSelectedMovie(movieId1.label);
     }
   };
 
@@ -120,7 +163,7 @@ const ScheduleList = () => {
   };
 
   const columns = [
-    createColumn("Tên phim", "movieName", 200, "left"),
+    createColumn("Tên phim", "movieName", 200, false, undefined, "left"),
     createColumn("Ngày chiếu", "showDate", 150, false, renderDate),
     createColumn("Giờ chiếu", "showTime"),
     createColumn("Rạp chiếu", "cinemaName"),
@@ -137,8 +180,8 @@ const ScheduleList = () => {
             record={record}
             handleDelete={handleDeleteData}
             handleView={handleView}
-            showDelete={true}
-            showEdit={true}
+            showDelete={checked}
+            showEdit={checked}
             showView={true}
             itemName={"lịch chiếu"}
           />
@@ -171,18 +214,22 @@ const ScheduleList = () => {
       type: "select",
       options: listMovie,
     },
-    { field: "date", label: "Ngày chiếu" },
+    { field: "date", label: "Ngày chiếu", type: "datePicker" },
   ];
 
   const renderHeader = () => (
     <TableHeader
       onReload={handleReload}
-      filter={filter}
-      setFilter={setFilter}
-      handleSearch={handleSearch}
-      headerTitle={"Danh sách lịch chiếu phim cho từng phim"}
-      itemSearch={itemSearch}
+      headerTitle={
+        <>
+          Danh sách lịch chiếu{" "}
+          <span style={{ color: "#E38601" }}>{selectedMovie}</span> tại{" "}
+          <span style={{ color: "#E38601" }}>{selectedCinema}</span>
+        </>
+      }
       create={handleToPageCreate}
+      showCreate={checked}
+      showFuncOther={false}
     />
   );
 
@@ -193,7 +240,10 @@ const ScheduleList = () => {
       if (query.hasOwnProperty(key)) {
         const label = key;
         const value = query[key];
-        if (value) {
+        if (label === "date") {
+          console.log("value", value);
+          q += `&date=${value.format(FORMAT_DATE_SEND_SERVER)}`;
+        } else if (value) {
           q += `&${label}=${value}`;
         }
       }
@@ -225,6 +275,14 @@ const ScheduleList = () => {
   return (
     <>
       <Row gutter={[20, 20]}>
+        <Col span={24}>
+          <SearchList
+            itemSearch={itemSearch}
+            handleSearch={handleSearch}
+            setFilter={setFilter}
+            filter={filter}
+          />
+        </Col>
         <Col span={24}>
           <Table
             scroll={{
